43535078f450e4efbf4740b7f0d776b972ee6f67
Nova Rosa (Rachael) <rachaelrosaline@gmail.com>
2024-08-29 12:14:16 -0500

    follow TwinkleBearDev lesson 2

--- src/main.c
+++ src/main.c
@@ -2,17 +2,59 @@
 
 #include <SDL2/SDL.h>
 
-int main() {
-    if(SDL_Init(SDL_INIT_VIDEO)) {
-        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-        return 1;
+// TODO idk maybe figure out a better default than 1200x1200
+#define INIT_WIDTH 1200
+#define INIT_HEIGHT 1200
+
+void error(char *msg) {
+    fprintf(stderr, "%s: %s\n", msg, SDL_GetError());
+}
+
+SDL_Texture* load(char *path, SDL_Renderer *r) {
+    SDL_Texture *t;
+    SDL_Surface *s = SDL_LoadBMP(path);
+    if(s) {
+        t = SDL_CreateTextureFromSurface(r, s);
+        SDL_FreeSurface(s);
+        if(!t)
+            error("Error creating texture from image");
+    } else
+        error("Error loading image");
+
+    return t;
+}
+
+int ren(SDL_Texture *t, SDL_Renderer *r, int x, int y) {
+    SDL_Rect o;
+    o.x = x;
+    o.y = y;
+
+    int e = SDL_QueryTexture(t, NULL, NULL, &o.w, &o.h);
+    if(e) {
+        error("Unable to look up texture");
+        return e;
     }
 
+    e = SDL_RenderCopy(r, t, NULL, &o);
+    if(e)
+        error("Error copying texture into the rendering context");
+
+    return e;
+}
+
+int main() {
+    int e = SDL_Init(SDL_INIT_VIDEO);
+    if(e) {
+        error("Error initializing SDL");
+        return e;
+    }
+
+    // TODO make window resizable
     SDL_Window *w = SDL_CreateWindow("yay it worky",
-                                     100, 100, 640, 480,
-                                     SDL_WINDOW_RESIZABLE);
+                                     100, 100, INIT_WIDTH, INIT_HEIGHT,
+                                     0 /*SDL_WINDOW_RESIZABLE*/);
     if(!w) {
-        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+        error("Error creating window");
         SDL_Quit();
         return 1;
     }
@@ -21,54 +63,58 @@ int main() {
                                          SDL_RENDERER_ACCELERATED|
                                              SDL_RENDERER_PRESENTVSYNC);
     if(!r) {
-        fprintf(stderr,
-                "Error creating rendering context: %s\n",
-                SDL_GetError());
+        error("Error creating rendering context");
         SDL_DestroyWindow(w);
         SDL_Quit();
         return 1;
     }
 
-    SDL_Surface *bmp = SDL_LoadBMP("res/nova.bmp");
-    if(!bmp) {
-        fprintf(stderr, "Error loading image: %s\n", SDL_GetError());
+    SDL_Texture *a = load("res/jacey.bmp", r);
+    if(!a) {
         SDL_DestroyRenderer(r);
         SDL_DestroyWindow(w);
         SDL_Quit();
         return 1;
     }
 
-    SDL_Texture *t = SDL_CreateTextureFromSurface(r, bmp);
-    if(!t) {
-        fprintf(stderr,
-                "Error creating texture from image: %s\n",
-                SDL_GetError());
-        SDL_FreeSurface(bmp);
+    SDL_Texture *b = load("res/nova.bmp", r);
+    if(!b) {
+        SDL_DestroyTexture(a);
         SDL_DestroyRenderer(r);
         SDL_DestroyWindow(w);
         SDL_Quit();
         return 1;
     }
-
-    SDL_FreeSurface(bmp);
 
     // TwinkleBearDev describes this loop as "sleepy" 😭
     for(int i = 0; i < 3; i++) {
         if(SDL_RenderClear(r))
-            fprintf(stderr,
-                    "Error clearing the rendering context: %s\n",
-                    SDL_GetError());
+            error("Error clearing the rendering context");
 
-        if(SDL_RenderCopy(r, t, NULL, NULL))
-            fprintf(stderr,
-                    "Error copying texture into the rendering %s%s\n",
-                    "context: ",
-                    SDL_GetError());
+        int w, h;
+        if(SDL_QueryTexture(b, NULL, NULL, &w, &h))
+            error("Unable to look up texture");
+
+        ren(b, r, 0, 0);
+        ren(b, r, w, 0);
+        ren(b, r, 0, h);
+        ren(b, r, w, h);
+
+        if(SDL_QueryTexture(a, NULL, NULL, &w, &h))
+            error("Unable to look up texture");
+
+        // TODO probably dont assume the screen width is the default
+        // width for a resizable window lol
+        int x = INIT_WIDTH / 2 - w / 2;
+        int y = INIT_HEIGHT / 2 - h / 2;
+        ren(a, r, x, y);
 
         SDL_RenderPresent(r);
         SDL_Delay(1000);
     }
 
+    SDL_DestroyTexture(b);
+    SDL_DestroyTexture(a);
     SDL_DestroyRenderer(r);
     SDL_DestroyWindow(w);
     SDL_Quit();
